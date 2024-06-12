2226534c680c579be86719df948bfdc7c8042161
Akash Patel <117297200+luminarakash@users.noreply.github.com>
2024-06-12 08:39:12 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Random from "./components/Random"
+import Tag from "./components/Tag";
+
+export default function App() {
+  return (
+
+    <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
+      <h1 className=" bg-white rounded-lg w-5/6 text-center mt-[30px]
+       px-10 py-2 text-3xl font-bold ">
+      RANDOM GIFS</h1>
+      <div className="flex flex-col w-full items-center gap-y-10 mt-[20px]">
+        <Random />
+        <Tag />
+      </div>
+    </div>
+
+  );
+}
